022672afa01e7eda0e71b582f3742ede0f606f64
alek <wait82985@gmail.com>
2023-11-29 15:53:33 +0600

    problem solved

--- src/main/java/StockSpanner.java
+++ src/main/java/StockSpanner.java
@@ -1,11 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class StockSpanner {
+    private List<Integer> storage = new ArrayList<>();
+    private Stack<Integer> stack = new Stack<>();
 
     public StockSpanner() {
 
     }
 
     public int next(int price) {
-
+        storage.add(price);
+        while (!stack.isEmpty() && storage.get(stack.peek()) <= price){
+            stack.pop();
+        }
+        int span = stack.isEmpty() ? storage.size() : storage.size() -  1 - stack.peek();
+        stack.push(storage.size() - 1);
+        return span;
     }
 }
 
--- src/test/java/StockSpannerTest.java
+++ src/test/java/StockSpannerTest.java
@@ -1,8 +1,26 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class StockSpannerTest {
     @Test
     public void test1(){
+        StockSpanner stockSpanner = new StockSpanner();
+        Assert.assertEquals(1, stockSpanner.next(100));
+        Assert.assertEquals(1, stockSpanner.next(80));
+        Assert.assertEquals(1, stockSpanner.next(60));
+        Assert.assertEquals(2, stockSpanner.next(70));
+        Assert.assertEquals(1, stockSpanner.next(60));
+        Assert.assertEquals(4, stockSpanner.next(75));
+        Assert.assertEquals(6, stockSpanner.next(85));
+    }
+    @Test
+    public void test2(){
+        StockSpanner stockSpanner = new StockSpanner();
+        Assert.assertEquals(1, stockSpanner.next(31));
+        Assert.assertEquals(2, stockSpanner.next(41));
+        Assert.assertEquals(3, stockSpanner.next(48));
+        Assert.assertEquals(4, stockSpanner.next(59));
+        Assert.assertEquals(5, stockSpanner.next(79));
 
     }
 }
